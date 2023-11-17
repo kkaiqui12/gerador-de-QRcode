28d984a43e779cf7cf9b7b70e2a0b6331417e0e1
Kaiqui <kkaiqui.12@gmail.com>
2023-11-17 20:47:34 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img")
+
+// Eventos
+
+// Gerar QR Code
+function genereteQrCode() {
+  const qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerHTML = "Gerando código..."
+  
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerHTML = "Código criado!"
+  })
+
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  genereteQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if(e.code === "Enter") {
+    genereteQrCode();
+  }
+});
+
+// Limpar área do QRCode
+qrCodeInput.addEventListener("keyup", () => {
+  if(!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerHTML = "Gerar QR Code"
+  }
+})
